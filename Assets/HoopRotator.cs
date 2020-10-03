aee36136ec1de3cdec3ee2b7f0c5c58832a63808
misaj <misaj@nordeus.com>
2020-10-03 14:23:25 +0200

    hoop lol

--- Assets/HoopRotator.cs
+++ Assets/HoopRotator.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HoopRotator : MonoBehaviour
 {
+
+    public Transform player;
+    public float Speed;
+    public float RotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class HoopRotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, RotationSpeed);
+
+        var translation = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        translation *= Speed * Time.deltaTime;
+        player.Translate(translation);
     }
 }
