7f61268c3587431c4103510e9207e9699e909e78
Djordje Cvijic <djordjec@nordeus.com>
2020-10-03 15:50:32 +0200

    TileData

--- Assets/Scripts/Settings/LevelData.cs
+++ Assets/Scripts/Settings/LevelData.cs
@@ -8,4 +8,19 @@ public class LevelData : ScriptableObject
     public int Width = 10;
     public int Height = 10;
     public TileType[] Tiles;
+
+    public TileData GetTile(int index)
+    {
+        return new TileData
+        {
+            Type = Tiles[index], X = index % Width, Y = index / Width;
+        };
+    }
+}
+
+public struct TileData
+{
+    public int X;
+    public int Y;
+    public TileType Type;
 }
