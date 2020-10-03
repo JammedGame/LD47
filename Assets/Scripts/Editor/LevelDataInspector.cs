af063b3bffec5e29d6ab8b305a668ff146236708
Djordje Cvijic <djordjec@nordeus.com>
2020-10-03 15:51:17 +0200

    compile errors

--- Assets/Scripts/Editor/LevelDataInspector.cs
+++ Assets/Scripts/Editor/LevelDataInspector.cs
@@ -1,34 +1,32 @@
-
-using System;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(LevelData))]
 public class LevelDataInspector : Editor
 {
-    public LevelData levelData;
+	public LevelData levelData;
 
-    private void OnEnable()
-    {
-        levelData = target as LevelData;
-    }
+	private void OnEnable()
+	{
+		levelData = target as LevelData;
+	}
 
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
+	private void OnSceneGUI()
+	{
+		for (var i = 0; i < levelData.Tiles.Length; i++)
+		{
+			var tileData = levelData.GetTile(i);
+		}
 
-        if (GUILayout.Button("CAO"))
-        {
-            
-        }
-    }
+		// Gizmos.DrawGUITexture()
+	}
 
-    private void OnSceneGUI()
-    {
-        for (var i = 0; i < levelData.Tiles.Length; i++)
-        {
-            var tileData = levelData.GetTile(i);
-        }
-        // Gizmos.DrawGUITexture()
-    }
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		if (GUILayout.Button("CAO"))
+		{
+		}
+	}
 }
--- Assets/Scripts/Settings/LevelData.cs
+++ Assets/Scripts/Settings/LevelData.cs
@@ -1,26 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu]
 public class LevelData : ScriptableObject
 {
-    public int Width = 10;
-    public int Height = 10;
-    public TileType[] Tiles;
+	public int Width = 10;
+	public int Height = 10;
+	public TileType[] Tiles;
 
-    public TileData GetTile(int index)
-    {
-        return new TileData
-        {
-            Type = Tiles[index], X = index % Width, Y = index / Width;
-        };
-    }
+	public TileData GetTile(int index)
+	{
+		return new TileData
+		{
+			Type = Tiles[index], X = index % Width, Y = index / Width
+		};
+	}
 }
 
 public struct TileData
 {
-    public int X;
-    public int Y;
-    public TileType Type;
+	public int X;
+	public int Y;
+	public TileType Type;
 }
