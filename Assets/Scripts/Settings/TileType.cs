9dbf2b339b548136fac93f06ace27b2321ba6212
Djordje Cvijic <djordjec@nordeus.com>
2020-10-03 16:27:50 +0200

    tiletype loadtexture

--- Assets/Scripts/Settings/TileType.cs
+++ Assets/Scripts/Settings/TileType.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public enum TileType
 {
-    Undefined = 0,
-    Rail = 1
+	Undefined = 0,
+	Rail = 1
+}
+
+public static class TileTypeExtensions
+{
+	public static Texture LoadTexture(this TileType type)
+	{
+		return Resources.Load<Texture>($"Textures/Tile{type}");
+	}
 }
