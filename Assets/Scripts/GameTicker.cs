16a2139a6eb1ea2a23990e1583d74731e9861256
misaj <misaj@nordeus.com>
2020-10-04 22:49:05 +0200

    fix collision between layers

--- Assets/Scripts/GameTicker.cs
+++ Assets/Scripts/GameTicker.cs
@@ -26,6 +26,9 @@ public class GameTicker : MonoBehaviour
 
     void Update()
     {
+        if (inputController == null)
+            return;
+
         inputController.ProcessInput();
         gameWorld.Tick(1f / 60f);
         viewController.Render();
