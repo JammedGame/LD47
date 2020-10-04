d70d0aec866c5139938a061d9cbfd5e74331c792
Djordje Cvijic <djordjec@nordeus.com>
2020-10-04 14:03:22 +0200

    level editor more improvements

--- Assets/Scripts/Editor/LevelEditorWindow.cs
+++ Assets/Scripts/Editor/LevelEditorWindow.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 public class LevelEditorWindow : EditorWindow
 {
 	private Vector2 brushScrollPos = Vector2.zero;
+	private bool magnifierActive;
+	private Vector2 magnifierPos;
+	private bool pipetteActive;
 	private Vector2 scrollPos = Vector2.zero;
+
 	private TileType tileBrush;
+
+	private TileType TileBrush
+	{
+		get => tileBrush;
+		set
+		{
+			tileBrush = value;
+			magnifierActive = pipetteActive = false;
+		}
+	}
 
 	public LevelData Data { get; set; }
 
@@ -20,20 +34,46 @@ public class LevelEditorWindow : EditorWindow
 		var defaultBrushStyle = new GUIStyle(GUI.skin.box) {padding = new RectOffset(3, 3, 3, 3)};
 		var activeBrushStyle = new GUIStyle(GUI.skin.box) {padding = new RectOffset(0, 0, 0, 0)};
 		var tileStyle = new GUIStyle(GUI.skin.box) {padding = new RectOffset(0, 0, 0, 0)};
+		var x = 0;
+		var y = 0;
 
-		GUI.Label(new Rect(0, 0, 50, 50), "Brush:");
+		var magnifier = GUI.Button(new Rect(x, y, 25, 25), Resources.Load<Texture>("Textures/Magnifier"),
+			magnifierActive ? activeBrushStyle : defaultBrushStyle);
+		if (magnifier)
+		{
+			magnifierActive = true;
+			pipetteActive = false;
+		}
+
+		x += 25;
+		GUI.Label(new Rect(x, y, 100, 25), magnifierPos.ToString());
+
+		x = 0;
+		y += 25;
+		var pipette = GUI.Button(new Rect(x, y, 25, 25), Resources.Load<Texture>("Textures/Pipette"),
+			pipetteActive ? activeBrushStyle : defaultBrushStyle);
+		if (pipette)
+		{
+			magnifierActive = false;
+			pipetteActive = true;
+		}
+
+		y += 50;
 		var brushes = GameSettings.Instance.SettingsPerType;
-		var y = 50;
-		brushScrollPos = GUI.BeginScrollView(new Rect(0, y, 50, 600), brushScrollPos,
-			new Rect(0, y, 50, 25 * (1 + brushes.Count)));
+		GUI.Label(new Rect(x, y, 50, 25), "Brush:");
+		y += 25;
+		brushScrollPos = GUI.BeginScrollView(new Rect(x, y, 50, 600), brushScrollPos,
+			new Rect(x, y, 50, 25 * (1 + brushes.Count)));
 		foreach (var brush in brushes)
 		{
-			var style = brush.TileType == tileBrush ? activeBrushStyle : defaultBrushStyle;
-			var brushPos = new Rect(0, y, 25, 25);
+			var style = !magnifierActive && !pipetteActive && brush.TileType == TileBrush
+				? activeBrushStyle
+				: defaultBrushStyle;
+			var brushPos = new Rect(x, y, 25, 25);
 			var (tex, rotate) = brush.TileType.LoadTexture();
 			GUIUtility.RotateAroundPivot(rotate.ToAngle(), brushPos.center);
 			var brushButton = GUI.Button(brushPos, tex, style);
-			if (brushButton) tileBrush = brush.TileType;
+			if (brushButton) TileBrush = brush.TileType;
 
 			GUI.matrix = Matrix4x4.identity;
 			y += 25;
@@ -41,20 +81,34 @@ public class LevelEditorWindow : EditorWindow
 
 		GUI.EndScrollView();
 
-		GUI.Label(new Rect(100, 0, 50, 50), "Level:");
-		scrollPos = GUI.BeginScrollView(new Rect(100, 50, 800, 600), scrollPos,
-			new Rect(100, 50, 25 * (1 + Data.Width), 25 * (1 + Data.Height)));
+		x = 125;
+		y = 75;
+		GUI.Label(new Rect(x, y, 50, 25), "Level:");
+		y += 25;
+		scrollPos = GUI.BeginScrollView(new Rect(x, y, 800, 600), scrollPos,
+			new Rect(x, y, 25 * (1 + Data.Width), 25 * (1 + Data.Height)));
 		for (var i = 0; i < Data.Tiles.Length; i++)
 		{
 			var tile = Data.GetTile(i);
-			var buttonPos = new Rect(100 + tile.X * 25, 50 + tile.Y * 25, 25, 25);
+			var buttonPos = new Rect(x + tile.X * 25, y + tile.Y * 25, 25, 25);
 			var (tex, rotate) = tile.Type.LoadTexture();
 			GUIUtility.RotateAroundPivot(rotate.ToAngle(), buttonPos.center);
 			var button = GUI.Button(buttonPos, tex, tileStyle);
 			if (button)
 			{
-				Data.SetTileType(i, tileBrush);
-				EditorUtility.SetDirty(Data);
+				if (magnifierActive)
+				{
+					magnifierPos = new Vector2(tile.X, tile.Y);
+				}
+				else if (pipetteActive)
+				{
+					TileBrush = tile.Type;
+				}
+				else
+				{
+					Data.SetTileType(i, TileBrush);
+					EditorUtility.SetDirty(Data);
+				}
 			}
 
 			GUI.matrix = Matrix4x4.identity;
