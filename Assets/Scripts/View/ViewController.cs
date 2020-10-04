7ba07d331809b93b3e36a4945a46bf38118af794
MisaJovanovic <misaj@nordeus.com>
2020-10-04 13:00:48 +0200

    train views

--- Assets/Scripts/View/ViewController.cs
+++ Assets/Scripts/View/ViewController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ViewController
@@ -5,22 +6,30 @@ public class ViewController
 	private readonly GameWorld gameWorld;
 	private readonly TileView[,] tile2View;
 	private readonly TileView tileViewPrefab;
+	private readonly List<TrainView> allTrainViews = new List<TrainView>();
 
 	public ViewController(GameWorld gameWorld)
 	{
 		this.gameWorld = gameWorld;
-		tileViewPrefab = Resources.Load<TileView>("Prefabs/TileView");
 
+		tileViewPrefab = Resources.Load<TileView>("Prefabs/TileView");
 		tile2View = new TileView[levelData.Width, levelData.Height];
 		for (var i = 0; i < levelData.Width; i++)
 		for (var j = 0; j < levelData.Height; j++)
 			tile2View[i, j] = TileView.CreateView(gameWorld.GetTile(i, j), tileViewPrefab);
+
+		foreach(var train in gameWorld.AllTrains)
+			allTrainViews.Add(TrainView.CreateView(train));
 	}
 
 	private LevelData levelData => gameWorld.LevelData;
 
 	public void Render()
 	{
+		foreach(var trainView in allTrainViews)
+		{
+			trainView.Update();
+		}
 	}
 }
 
