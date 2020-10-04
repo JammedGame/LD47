c9b8214447bfe813e19af0980b29a846d61ae99f
Djordje Cvijic <djordjec@nordeus.com>
2020-10-04 12:41:36 +0200

    train settings & more level data

--- Assets/Scripts/Settings/LevelData.cs
+++ Assets/Scripts/Settings/LevelData.cs
@@ -1,10 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 [CreateAssetMenu]
 public class LevelData : ScriptableObject
 {
 	public int Width = 10;
 	public int Height = 10;
+	public float VictoryAfterSeconds;
+	public int VictoryAfterCargoCollected;
+	public float DefeatAfterSeconds;
+	public int DefeatAfterCargoOverflow;
+	public float DefaultCargoDespawnAfterSeconds;
+	public int DefeatAfterCargoDespawned;
+	[Table] public List<TrainSpawn> TrainSpawns;
+	[Table] public List<CargoSpawn> CargoSpawns;
 	public TileType[] Tiles;
 
 	public TileData GetTile(int index)
@@ -26,4 +36,49 @@ public struct TileData
 	public int X;
 	public int Y;
 	public TileType Type;
+}
+
+[Serializable]
+public struct TrainSpawn
+{
+	public int X;
+	public int Y;
+	public Rotation Rotation;
+	public TrainType Type;
+	public TrainColor Color;
+	public int InitialCars;
+}
+
+public enum TrainType
+{
+	Undefined = 0,
+	Slow = 1,
+	Fast = 2,
+	FastButLikeEvenMoreFasterYouKnow = 3
+}
+
+public enum TrainColor
+{
+	Undefined = 0,
+	Cyan = 1,
+	Magenta = 2,
+	Yellow = 3,
+	BioShockSepiaAesthetic = 4
+}
+
+[Serializable]
+public struct CargoSpawn
+{
+	public int X;
+	public int Y;
+	public float SpawnsAtSeconds;
+	public float DespawnsAfterSeconds;
+	public CargoType Type;
+	public TrainColor Color;
+}
+
+public enum CargoType
+{
+	Default = 0,
+	NewCar = 1
 }
