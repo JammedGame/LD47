46021f6ef694e84fa0f810d45aff82e1570a89f9
MisaJovanovic <misaj@nordeus.com>
2020-10-05 23:13:21 +0200

    new colors

--- Assets/Scripts/InGameUI/WagonCollectionCounterUI.cs
+++ Assets/Scripts/InGameUI/WagonCollectionCounterUI.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.UI;
 
 public class WagonCollectionCounterUI : InGameUIObject
 {
@@ -23,6 +24,7 @@ public class WagonCollectionCounterUI : InGameUIObject
 		}
 
 		UpdateText();
+		GetComponentInChildren<Image>().color = GameSettings.Instance.GetColorSettings(TrainColor).Color;
 	}
 
 	public void UpdateText()
