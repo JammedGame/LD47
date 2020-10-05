3a2f8218b8970fb11f075bddc229d23c2e73b98f
MisaJovanovic <misaj@nordeus.com>
2020-10-05 22:36:49 +0200

    winnerIsYouControls

--- Assets/Scripts/InGameUI/InGameUIController.cs
+++ Assets/Scripts/InGameUI/InGameUIController.cs
@@ -1,18 +1,28 @@
+using System;
 using UnityEngine;
 
 // main controller
 public class InGameUIController : MonoBehaviour
 {
+	[SerializeField] GameObject winnerIsYouControls;
+
 	GameWorld world;
 	InGameUIObject[] allUIControls;
 
 	public void Initialize(GameWorld gameWorld)
 	{
 		this.world = gameWorld;
+		this.world.OnVictory += OnVictory;
 		this.allUIControls = GameObject.FindObjectsOfType<InGameUIObject>();
+		this.winnerIsYouControls.SetActive(false);
 
 		foreach(var obj in allUIControls)
 			obj.OnInitialize(world);
+	}
+
+	private void OnVictory()
+	{
+		winnerIsYouControls.SetActive(true);
 	}
 
 	public void OnUpdate()
