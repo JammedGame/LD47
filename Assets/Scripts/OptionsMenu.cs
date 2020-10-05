4af75bf92c9832fc47575a33d8ca637a3eeaf78b
Djordje Cvijic <djordjec@nordeus.com>
2020-10-06 00:35:59 +0200

    options menu

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+	public TextMeshProUGUI ToggleMusicLabel;
+	public TextMeshProUGUI ToggleSoundLabel;
+
+	private void Start()
+	{
+		UpdateLabels();
+	}
+
+	public void ToggleMusic()
+	{
+		SoundManager.Instance.MusicEnabled = !SoundManager.Instance.MusicEnabled;
+		UpdateLabels();
+	}
+
+	public void ToggleSound()
+	{
+		SoundManager.Instance.SoundEnabled = !SoundManager.Instance.SoundEnabled;
+		UpdateLabels();
+	}
+
+	private void UpdateLabels()
+	{
+		ToggleMusicLabel.text = SoundManager.Instance.MusicEnabled ? "MUTE MUSIC" : "UNMUTE MUSIC";
+		ToggleSoundLabel.text = SoundManager.Instance.SoundEnabled ? "MUTE SOUND" : "UNMUTE SOUND";
+	}
+}
