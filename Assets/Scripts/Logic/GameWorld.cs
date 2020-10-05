f1246bb6ba916fd4aad38a9267772f4f81714efb
MisaJovanovic <misaj@nordeus.com>
2020-10-05 22:30:00 +0200

    VictoryDeclared

--- Assets/Scripts/Logic/GameWorld.cs
+++ Assets/Scripts/Logic/GameWorld.cs
@@ -8,16 +8,14 @@ public class GameWorld
 	private readonly Tile[,] tiles;
 	public List<Train> AllTrains { get; } = new List<Train>();
 	public float SecondsElapsed { get; private set; }
+	public readonly Dictionary<TrainColor, int> NeededScore = new Dictionary<TrainColor, int>();
 	public readonly Dictionary<TrainColor, int> CollectedCarsScore = new Dictionary<TrainColor, int>();
+	public bool VictoryDeclared { get; private set; }
+	public bool DefeatDeclared { get; private set; }
 
 	public event Action OnScoreUpdated;
-
-	public void CollectCars(TrainColor color, int cars)
-	{
-		CollectedCarsScore.TryGetValue(color, out int score);
-		CollectedCarsScore[color] = score + cars;
-		OnScoreUpdated?.Invoke();
-	}
+	public event Action OnVictory;
+	public event Action OnDefeat;
 
 	public GameWorld(LevelData levelData)
 	{
@@ -41,7 +39,39 @@ public class GameWorld
 		{
 			var tile = tiles[cargoSpawn.X, cargoSpawn.Y];
 			tile.AddCargoSpawn(cargoSpawn);
+
+			NeededScore.TryGetValue(cargoSpawn.Color, out int currentScore);
+			NeededScore[cargoSpawn.Color] = currentScore + 1;
 		}
+	}
+
+	public void CollectCars(TrainColor color, int cars)
+	{
+		CollectedCarsScore.TryGetValue(color, out int score);
+		CollectedCarsScore[color] = score + cars;
+		OnScoreUpdated?.Invoke();
+		CheckVictory();
+	}
+
+	public void CheckVictory()
+	{
+		if (VictoryDeclared)
+			return;
+
+		foreach(var score in NeededScore)
+		{
+			CollectedCarsScore.TryGetValue(score.Key, out int currentlyCollected);
+			if (currentlyCollected < score.Value)
+				return;
+		}
+
+		OnVictory?.Invoke();
+		VictoryDeclared = true;
+	}
+
+	public void CheckDefeat()
+	{
+
 	}
 
 	public void RegisterSpawner(CargoSpawner cargoSpawner)
