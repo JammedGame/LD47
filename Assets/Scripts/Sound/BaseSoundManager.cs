2b04cd10b78fe96341f1b64d362f7174e782c180
Djordje Cvijic <djordjec@nordeus.com>
2020-10-13 23:43:34 +0200

    sound improvements

---
+++ Assets/Scripts/Sound/BaseSoundManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class BaseSoundManager<T> : MonoBehaviour where T : BaseSoundManager<T>
+{
+	private static BaseSoundManager<T> _instance;
+
+	public AudioSource musicSource;
+	public AudioSource soundSource;
+
+	public static T Instance => (T) _instance;
+
+	public bool SoundEnabled
+	{
+		get => !soundSource.mute;
+		set => soundSource.mute = !value;
+	}
+
+	public bool MusicEnabled
+	{
+		get => !musicSource.mute;
+		set => musicSource.mute = !value;
+	}
+
+	private void Awake()
+	{
+		if (_instance == null)
+		{
+			_instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else if (_instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void Start()
+	{
+		SoundEnabled = true;
+		MusicEnabled = true;
+	}
+
+	private void OnDestroy()
+	{
+		if (_instance == this) _instance = null;
+	}
+
+	protected void PlayMusic(AudioClip clip)
+	{
+		if (musicSource.clip == clip && musicSource.isPlaying) return;
+
+		musicSource.Stop();
+		if (clip == null) return;
+
+		musicSource.loop = true;
+		musicSource.clip = clip;
+		musicSource.Play();
+	}
+
+	protected void PlaySound(AudioClip clip)
+	{
+		if (soundSource.isPlaying) return;
+
+		soundSource.Stop();
+		if (clip == null) return;
+
+		soundSource.loop = false;
+		soundSource.clip = clip;
+		soundSource.Play();
+	}
+}
---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class SoundManager : BaseSoundManager<SoundManager>
+{
+	[Header("Music")] public AudioClip MusicMainMenu;
+	public AudioClip MusicGame;
+
+	[Header("Sound")] public AudioClip SoundTrackSwitch;
+	public AudioClip SoundCargoPickUp;
+	public AudioClip SoundCargoDropOff;
+	public AudioClip SoundTrainStop;
+	public AudioClip SoundTrainStart;
+	public AudioClip SoundVictory;
+	public AudioClip SoundDefeat;
+
+	public void PlayMusicMainMenu()
+	{
+		PlayMusic(MusicMainMenu);
+	}
+
+	public void PlayMusicGame()
+	{
+		PlayMusic(MusicGame);
+	}
+
+	public void PlaySoundTrackSwitch()
+	{
+		PlaySound(SoundTrackSwitch);
+	}
+
+	public void PlaySoundCargoPickUp()
+	{
+		PlaySound(SoundCargoPickUp);
+	}
+
+	public void PlaySoundCargoDropOff()
+	{
+		PlaySound(SoundCargoDropOff);
+	}
+
+	public void PlaySoundTrainStop()
+	{
+		PlaySound(SoundTrainStop);
+	}
+
+	public void PlaySoundTrainStart()
+	{
+		PlaySound(SoundTrainStart);
+	}
+
+	public void PlaySoundVictory()
+	{
+		PlaySound(SoundVictory);
+	}
+
+	public void PlaySoundDefeat()
+	{
+		PlaySound(SoundDefeat);
+	}
+}
